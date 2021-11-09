4aba43fc5369fab2092690ffdef22e9eeeb5c478
ZhengQ2 <77452278+ZhengQ2@users.noreply.github.com>
2021-11-09 10:35:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+# 
+# MIT License
+#
+# Copyright (c) 2021 Zheng Qiu
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
 import math
 
 print("Please enter the larger number")
@@ -11,7 +34,7 @@ r = x*a+y*b
 q = 0
 q0 = q
 
-print("x\t\ty\t\tr\t\tq\n" + str(x) +"\t\t" + str(y) + "\t\t" + str(r) + "\t\t" + str(q0) +"\n")
+print("x\t\ty\t\tr\t\tq\n" + str(x) +"\t\t" + str(y) + "\t\t" + str(r) + "\t\t" + str(q0))
 x1 = x
 y1 = y
 
@@ -22,7 +45,7 @@ q = 1
 q0 = q
 r0 = r
 
-print(str(x) +"\t\t" + str(y) + "\t\t" + str(r) + "\t\t" + str(q0) +"\n")
+print(str(x) +"\t\t" + str(y) + "\t\t" + str(r) + "\t\t" + str(q0))
 
 x2 = 0
 y2 = 0
@@ -39,4 +62,4 @@ while r != 0:
     x = (0-q) * x1+x2
     y = (0-q) * y1+y2
     r = x * a+y * b
-    print(str(x) +"\t\t" + str(y) + "\t\t" + str(r) + "\t\t" + str(q0) +"\n")
+    print(str(x) +"\t\t" + str(y) + "\t\t" + str(r) + "\t\t" + str(q0))
