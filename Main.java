5b5f92688bcdd2e564bc7bb11531d70e38b732d6
ZhengQ2 <77452278+ZhengQ2@users.noreply.github.com>
2021-11-09 23:38:35 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package idet.calculator;
+package ldet.calculator;
 
 import java.util.Scanner;
 
