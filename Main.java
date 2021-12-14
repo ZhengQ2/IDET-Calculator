7beeff1524c9e7f2c06d324fed22a41cb99187a6
ZhengQ2 <77452278+ZhengQ2@users.noreply.github.com>
2021-12-14 11:45:11 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -21,46 +21,41 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package ldet.calculator;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Zheng Qiu
- */
 public class Main {
-    
+
     public static void main(String[] args){
         System.out.println("Please enter the larger number");
         Scanner s = new Scanner(System.in);
         int a = s.nextInt();
         System.out.println("Please enter the smaller number");
         int b = s.nextInt();
-        
+
         int x = 1;
         int y = 0;
         int r = x*a+y*b;
         int q = 0;
         int q0 = q;
-        
+
         System.out.println("x\t\ty\t\tr\t\tq\n"
                 + x +"\t\t" + y + "\t\t" + r + "\t\t" + q0);
         int x1 = x;
         int y1 = y;
         int r0 = r;
-        
+
         x = 0;
         y = 1;
         r = x*a+y*b;
         q = 1;
         q0 = q;
-        
+
         System.out.println(x +"\t\t" + y + "\t\t" + r + "\t\t" + q0);
-        
+
         int x2 = 0;
         int y2 = 0;
-        
+
         while(r != 0){
             q = (int) Math.floor(r0/r);
             x2 = x1;
@@ -69,11 +64,13 @@ public class Main {
             y1 = y;
             q0 = q;
             r0 = r;
-            
+
             x = (0-q)*x1+x2;
             y = (0-q)*y1+y2;
             r = x*a+y*b;
             System.out.println(x +"\t\t" + y + "\t\t" + r + "\t\t" + q0);
         }
+
+        System.out.println("Thus, gcd("+a+","+b+") = "+r0+", when x = " +x1+" and y = "+y1);
     }
 }
